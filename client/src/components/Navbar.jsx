9ed557302fb1b4ac0c608066fb083736a0e1c798
Abdul Ahad Butt <ahadbutt.pk@gmail.com>
2020-07-10 14:04:57 +0500

    Started to add icons to the menu
    This build is still in the developing phase

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,23 +1,67 @@
-import React, {Component} from 'react';
-import hamburger from '../icons/hamburger_menu.svg'
+import React, {Component, useState} from 'react';
+import hamburger from '../icons/hamburger_menu.svg';
+import github from '../icons/icons8-github.svg';
+import home from '../icons/icons8-home.svg';
 
 class NavBar extends Component {
 
+
+    state = {
+        open: false
+    }
+
+
     handleMenu = () => {
-        console.log(`Need to implement a function to handle menu in NavBar`)
+        this.setState({
+            open: !this.state.open
+        })
     }
 
     render() {
+        // Get all the icons and pre process here or in component did mount
+
+
+
+
         return (
             <nav className='nav-bar'>
                 <h1 className="blog-title">Ahad's Mundane Adventures</h1>
                 <button className='menu-button' onClick={this.handleMenu}>
                     <img src={hamburger} alt="menu-button"/>
                 </button>
+                {this.state.open && <Menu/>}
             </nav>
         );
     }
 }
+
+class Menu extends Component {
+    
+
+    render() {
+
+        return(
+            <div className="menu">
+                <ul>
+                    <NavItem icon={home} link=''>Home</NavItem>
+                </ul>
+            </div>
+        )
+    }
+}
+
+function NavItem(props) {
+    return(
+        <li className="nav-item">
+            <a href={props.link} className="nav-icon">
+                
+                {props.children}
+            </a>
+        </li>
+    )
+}
+
+
 
 
 export default NavBar;
